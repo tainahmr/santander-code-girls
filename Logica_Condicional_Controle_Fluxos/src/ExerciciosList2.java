8d500e51c86d6347721946a3eac2759fcae1af21
Tainah <tainahmr@gmail.com>
2022-08-02 17:12:42 -0300

    Exercicio 2

--- Logica_Condicional_Controle_Fluxos/src/ExerciciosList2.java
+++ Logica_Condicional_Controle_Fluxos/src/ExerciciosList2.java
@@ -1,3 +1,59 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class ExerciciosList2 {
-    
+/*Utilizando listas, faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+1. "Telefonou para a vítima?"
+2. "Esteve no local do crime?"
+3. "Mora perto da vítima?"
+4. "Devia para a vítima?"
+5. "Já trabalhou com a vítima?"
+Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como
+"Cúmplice" e 5 como "Assassina". Caso contrário, ele será classificado como "Inocente".*/
+    public static void main(String[] args){
+        ArrayList<String> respostas = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Perguntas: ");
+        System.out.print("\nTelefonou para vítima? ");
+        String resposta = scan.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.println("Esteve no local do crime? ");
+        resposta = scan.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.println("Mora perto da vítima? ");
+        resposta = scan.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.println("Devia para vítima? ");
+        resposta = scan.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.println("Já trabalhou com a vítima? ");
+        resposta = scan.next();
+        respostas.add(resposta.toLowerCase());
+
+        System.out.println(respostas);
+        scan.close();
+
+        int count = 0;
+        Iterator<String> contador = respostas.iterator();
+        while(contador.hasNext()){
+            String resp = contador.next();
+            if(resp.contains("s")){
+                count++;
+            }
+        }
+
+        switch(count){
+            case 2:
+                System.out.println("SUSPEITA!"); break;
+            case 3:
+            case 4:
+                System.out.println("CÚMPLICE!"); break;
+            case 5:
+                System.out.println("ASSASSINO!"); break;
+            default:
+                System.out.println("INOCENTE!"); break;
+        }
+    }
 }
