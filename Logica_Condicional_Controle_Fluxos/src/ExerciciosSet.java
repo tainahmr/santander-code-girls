a014d142cce1c527fc920a3478f1feba7d4fefdf
Tainah <tainahmr@gmail.com>
2022-08-02 20:57:32 -0300

    exercicio set

---
+++ Logica_Condicional_Controle_Fluxos/src/ExerciciosSet.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Collections;;
+
+public class ExerciciosSet {
+/*Crie uma conjunto contendo as cores do arco-íris e:
+a) Exiba todas as cores o arco-íris uma abaixo da outra;
+b) A quantidade de cores que o arco-íris tem;
+c) Exiba as cores em ordem alfabética;
+d) Exiba as cores na ordem inversa da que foi informada;
+e) Exiba todas as cores que começam com a letra ”v”;
+f) Remova todas as cores que não começam com a letra “v”;
+g) Limpe o conjunto;
+h) Confira se o conjunto está vazio;*/
+    public static void main(String[] args){
+        System.out.println("Crie uma conjunto contendo as cores do arco-íris: ");
+        HashSet<String> coresArcoIris = new HashSet<>(Arrays.asList("violeta", "anil", "azul", "verde", "amarelo", "laranja", "vermelho"));
+        System.out.println(coresArcoIris);
+
+        System.out.println("Exiba todas as cores do arco-iris uma abaixo da outra: ");
+        for (String cor : coresArcoIris){
+            System.out.println(cor);
+        }
+        System.out.println("A quantidade de cores que o arco-iris tem: " + coresArcoIris.size());
+
+        System.out.println("Exiba as cores na ordem inversa da que foi informada");
+        LinkedHashSet<String> coresArcoIris3 = new LinkedHashSet<>(Arrays.asList("violeta", "anil", "azul", "verde", "amarelo", "laranja", "vermelho"));
+        System.out.println(coresArcoIris3);
+        ArrayList<String> coresArcoIrisList = new ArrayList<>(coresArcoIris3);
+        Collections.reverse(coresArcoIrisList);
+        System.out.println(coresArcoIrisList);
+
+        System.out.println("Exiba todas as cores que começam com a letra ”v”: ");
+        for (String cor: coresArcoIris) {
+            if(cor.startsWith("v")) System.out.println(cor);
+
+        }
+        System.out.println("Remova todas as cores que não começam com a letra “v”: ");
+        Iterator<String> iterator2 = coresArcoIris.iterator();
+        while (iterator2.hasNext()) {
+            if (!iterator2.next().startsWith("v")) iterator2.remove();
+
+    }
+    System.out.println(coresArcoIris);
+
+    System.out.println("Limpe o conjunto: ");
+    coresArcoIris.clear();
+
+    System.out.println("Confira se o conjunto está vazio: " + coresArcoIris.isEmpty());
+
+    }
+}
+    
+    
+
