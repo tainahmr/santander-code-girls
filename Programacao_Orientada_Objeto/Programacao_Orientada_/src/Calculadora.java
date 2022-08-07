dab8af9e190edcc732a4172e80bc17ac0c213da8
Tainah <tainahmr@gmail.com>
2022-08-07 19:35:24 -0300

    Interface

---
+++ Programacao_Orientada_Objeto/Programacao_Orientada_/src/Calculadora.java
@@ -0,0 +1,23 @@
+public class Calculadora implements OperacaoMatematica {
+
+    @Override
+    public void soma(double operando1, double operando2){
+        System.out.println("Soma: " + operando1 + operando2);
+    }
+
+    @Override
+    public void subtracao(double operando1, double operando2){
+        System.out.println("Subtração: " + (operando1 - operando2));
+    }
+    
+    @Override
+    public void multiplicacao(double operando1, double operando2){
+        System.out.println("Multiplicacao: " + operando1*operando2);
+    }
+
+    @Override
+    public void divisao(double operando1, double operando2){
+        System.out.println("Divisão: " + operando1/operando2);
+    }
+    
+}
---
+++ Programacao_Orientada_Objeto/Programacao_Orientada_/src/OperacaoMatematica.java
@@ -0,0 +1,6 @@
+interface OperacaoMatematica {
+    void soma(double operando1, double operando2);
+    void subtracao(double operando1, double operando2);
+    void multiplicacao(double operando1, double operando2);
+    void divisao(double operando1, double operando2);
+}
